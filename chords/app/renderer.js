e0d68d86fe2b338e730e4da87526ec42b6ccfe65
Raffaele Sena <raff367@gmail.com>
2017-04-12 21:20:04 -0700

    Added scales and playback

--- chords/app/renderer.js
+++ chords/app/renderer.js
@@ -3,13 +3,13 @@
 // All of the Node.js APIs are available in this process.
 
 var s11 = require('sharp11');
-// var audio = require('sharp11-web-audio');
+var audio = require('sharp11-web-audio');
 
 var VF = require('vexflow').Flow;
 
 var x = 20;
 var y = 20;
-var w = 180;
+var w = 260; // 180;
 var lb = VF.Barline.type.SINGLE;
 var ns = 0;
 
@@ -17,6 +17,11 @@ var vf = null;
 var score = null;
 var cw = 0;
 var ch = 0;
+
+var octave = 4;
+var outputType = 'chord';
+var notelist = [];
+var chordlist = [];
 
 initScore = function(sel) {
   cw = window.innerWidth - 60;
@@ -54,12 +59,13 @@ function system() {
     return s;
 }
 
-drawStave = function(text, notes) {
-  var stave = system().addStave({voices: [score.voice(score.notes(notes)).setStrict(false)]})
+function drawStave(text, notes) {
+  var stave = system().addStave({voices: [score.voice(score.notes(notes), {time: "8/4"}).setStrict(false)]})
     .setText(text, VF.Modifier.Position.ABOVE, {justification: VF.TextNote.Justification.LEFT});
 
   if (ns == 1) {
     stave.setBegBarType(lb);
+    stave.setClef('treble')
   } else if (ns == 4) {
     stave.setEndBarType(lb);
   }
@@ -67,22 +73,68 @@ drawStave = function(text, notes) {
   vf.draw();
 }
 
-addChord = function(ele) {
-  var cname = ele.value;
-  ele.value = '';
+changeOutputType = function(otype) {
+    outputType = otype;
+}
 
-  if (! cname) {
+addChord = function(inp, out) {
+  var clist;
+
+  if (typeof(inp)==='string') {
+     clist = inp.trim();
+  } else {
+      clist = inp.value.trim();
+      inp.value = '';
+  }
+
+  if (clist === '') {
     return;
   }
 
-  var c = s11.chord.create(cname, 4);
-  if (! c) {
-    console.log("invalid chord name", cname);
-    return;
-  }
+  clist.split(",").forEach(function(cname) {
+      var c = s11.chord.create(cname, octave);
+      if (! c) {
+        console.log("invalid chord name", cname);
+        return;
+      }
 
-  cname = c.root.name + c.symbol;
-  var snotes = c.chord.map(function(n, i) { return n.fullName + (i===0 ? '/q' : ''); }).join(",");
-  console.log(cname, snotes);
-  drawStave(cname, snotes);
+      notelist = notelist.concat(c.chord);
+
+      cname = c.root.name + c.symbol;
+      if (c.bass) {
+        cname += '/' + c.bass.fullName
+      }
+      chordlist.push(cname)
+
+      var snotes;
+
+      if (outputType === 'chord') {
+        snotes = c.chord.map(function(n, i) { return n.fullName + (i===0 ? '/q' : ''); }).join(",");
+      } else {
+        snotes = c.scale().inOctave(octave).scale.map(function(n, i) { return n.fullName + (i===0 ? '/q' : ''); }).join(",");
+      }
+
+      console.log(outputType, cname, snotes);
+      drawStave(cname, snotes);
+
+      if (out) {
+        out.innerText = chordlist.join(",")
+      }
+  });
+}
+
+playNotes = function(t) {
+    audio.init(function (err, fns) {
+        if (err != undefined) {
+            alert(err);
+        } else {
+            var n = Array.from(notelist);
+            if (t != 0) {
+                n = n.map(function(v) { return v.transposeDown("" + t); });
+            }
+
+            console.log(n.toString());
+            fns.arpeggiate(n, 0.5);
+        }
+    });
 }
